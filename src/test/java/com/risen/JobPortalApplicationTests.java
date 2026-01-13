cfd49e8cc47ac5fa96568ffa98d3465353898e58
Sneha Gupta <snhagpta2105@gmail.com>
2026-01-13 13:00:56 +0530

    project initialization

---
+++ src/test/java/com/risen/JobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.risen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
